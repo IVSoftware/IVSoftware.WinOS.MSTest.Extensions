e548f31451e9a4fe71eda7fc1e845baf92f230b7
Tom Gregor <tgregor@ivsoftware.com>
2026-05-01 10:06:28 -0600

    ORIGIN: XBoundObject witness 2.0.3

---
+++ TestBench.PublicContractLatest.MSTest/TestClass_PublicContract.cs
@@ -0,0 +1,11 @@
+﻿namespace TestBench.MSTest
+{
+    [TestClass]
+    public sealed class TestClass_PublicContract
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
